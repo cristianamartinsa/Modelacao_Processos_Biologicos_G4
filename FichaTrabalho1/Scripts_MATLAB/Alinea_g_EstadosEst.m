014b6c0c954f4ecfd9f6102ed4e0cc07b1a7f90c
cristianamartinsa <96478330+cristianamartinsa@users.noreply.github.com>
2022-05-13 22:17:43 +0100

    Update Alinea_g_EstadosEst.m

--- FichaTrabalho1/Scripts_MATLAB/Alinea_g_EstadosEst.m
+++ FichaTrabalho1/Scripts_MATLAB/Alinea_g_EstadosEst.m
@@ -1,6 +1,12 @@
 clc; clear all; close all;
 
-y0 = [0 0 0 0]; 
+y1 = [0 0 0 0]; 
 %setNL=@Alinea_g_Modelo;
 %y=fsolve(setNL, y0)
-[y, fy]=fsolve(@Alinea_g_Modelo, y0)
+[y_1, fy_1]=fsolve(@Alinea_g_Modelo, y1)
+
+
+y2 = [0 4 0 0]; 
+%setNL=@Alinea_g_Modelo;
+%y=fsolve(setNL, y0)
+[y_2, fy_2]=fsolve(@Alinea_g_Modelo, y2)
